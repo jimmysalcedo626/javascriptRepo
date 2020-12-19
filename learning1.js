d8511a8f234a8abfad26f6b8d87fb6a4e4b34402
jimmysalcedo626 <33579475+jimmysalcedo626@users.noreply.github.com>
2020-12-18 20:28:08 -0800

    Create learning1.js

---
+++ learning1.js
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Lesson 1</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+
+<h1>Learning Javascript</h1>
+
+  <script src="js/scripts.js">
+  
+  
+  
+  
+  
+  </script>
+</body>
+</html>
